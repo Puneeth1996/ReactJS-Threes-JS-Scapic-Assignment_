9e9b984e534f33bd9b5ff7a941b707a00b882501
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2019-04-04 14:55:14 +0530

    ReactJS ThreeJS 3d Models Shelf

---
+++ src/component/Food.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import "../App1.css"
+
+import $ from 'jquery';
+
+import foodList from './Food.json';
+
+class Food extends Component {    
+    
+
+  constructor(){
+    super()
+    this.state = {
+      list : foodList
+    } 
+    this.scroll = this.scroll.bind(this)
+  }
+
+  // works with values placed in the app1.css with className food
+  scroll(direction){
+    let far = $( '.food').width()/4*direction;
+    // console.log(far);       
+    // console.log($( '.food'));                   
+    let pos = $('.food').scrollLeft() + far;
+    // console.log(pos);
+    $('.food').animate( { scrollLeft: pos }, 2000)
+  }
+
+  render() {
+    return (        
+        // Making a demo component for Food component
+          <div className="container">            
+            <h2 className="heading">Food</h2>
+            <div className="wrapper">
+              <a className="prev" onClick={this.scroll.bind(null,-1)}>&#10094;</a>
+              <div className="food">
+                <div className="image">
+                  { foodList.map( (itm, index ) => 
+                    { return(
+                      <img src={itm.thumb} alt={itm.name} />                     
+                      )
+                      
+                    }
+                  )}
+                
+                </div>
+              </div>
+
+              <a className="next" onClick={this.scroll.bind(null,1)}>&#10095;</a>
+            </div>
+          </div>
+    );
+  }
+}
+
+export default Food;
---
+++ src/component/Furniture.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import "../App1.css"
+
+import $ from 'jquery';
+
+import furnitureList from './Furniture.json';
+
+class Furniture extends Component {    
+    
+
+  constructor(){
+    super()
+    this.state = {
+      list : furnitureList
+    } 
+    this.scroll = this.scroll.bind(this)
+  }
+
+  // works with values placed in the app1.css with className food
+  scroll(direction){
+    let far = $( '.Furniture').width()/6*direction;
+    // console.log(far);       
+    // console.log($( '.food'));                   
+    let pos = $('.Furniture').scrollLeft() + far;
+    // console.log(pos);
+    $('.Furniture').animate( { scrollLeft: pos }, 1500)
+  }
+
+  render() {
+    return (        
+        // Making a demo component for Food component
+          <div className="container">            
+            <h2 className="heading">Furniture</h2>
+            <div className="wrapper">
+              <a className="prev" onClick={this.scroll.bind(null,-1)}>&#10094;</a>
+              <div className="Furniture">
+                <div className="image">
+                  { furnitureList.map( (itm, index ) => 
+                    { return(
+                      <img src={itm.thumb} alt={itm.name} />                     
+                      )
+                      
+                    }
+                  )}
+                
+                </div>
+              </div>
+
+              <a className="next" onClick={this.scroll.bind(null,1)}>&#10095;</a>
+            </div>
+          </div>
+    );
+  }
+}
+
+export default Furniture;
